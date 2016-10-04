982a5cd5647dc2e92622a303c508bbe6cca150b3
tincanfossil <brucewhan@live.com.au>
2016-10-04 14:02:48 +1000

    push to heroku

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.1.0.rc2'
+gem 'rake', '11.2.2'
 
 group :development, :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # required for heroku
+  #set for your actual host
+  config.action_mailer.default_url_options = { :host => 'brucew-pinteresting.com' }
 end
