2c51cd9f88df18b66af201cd6a2ef5e9016d26e9
tincanfossil <brucewhan@live.com.au>
2016-11-11 14:59:38 +1000

    image not loading fix I hope

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,5 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300*300>", :thumb => "100*100"}
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/gif", "image/png"]
+	validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
 end
