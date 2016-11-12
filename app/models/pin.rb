025e6a6f1e054af7a25427d08e53ba8d6f2019ee
tincanfossil <brucewhan@live.com.au>
2016-11-12 11:31:11 +1000

    again with paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300*300>", :thumb => "100*100"}
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/gif", "image/png"]
+	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
 end
