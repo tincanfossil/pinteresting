088eb42656b93140bc85d2c2b25ddff5234832be
tincanfossil <brucewhan@live.com.au>
2016-11-12 13:48:47 +1000

    add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300*300>", :thumb => "100*100"}
 	validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+	validates :image, presence: true
+  validates :description, presence: true
 end
