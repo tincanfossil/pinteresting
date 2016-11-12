696372266987719a54daafb7e33a92485ebed8af
tincanfossil <brucewhan@live.com.au>
2016-11-12 16:49:10 +1000

    fix bug where edit delete links show pins without users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :pins
+  has_many :pins, dependent: :destroy
   validates :name, presence: true
 end
